7bf64e3d571ada2fdd9336caa66b78db70a4e5c0
omer <jagenau49@gmail.com>
2022-07-11 14:31:26 +0200

    api completed

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+
+const mongodbUri = "mongodb://localhost:27017";
+const client = new MongoClient(mongodbUri);
+// await client.connect();
+// const db = client.db("northwind");
+// const employees = await db.collection("employees").find().toArray();
+// console.log(8, employees.length);
 
 const app = express();
 const port = 3001;
@@ -7,6 +15,19 @@ app.get("/", (req, res) => {
   res.send("Welcome to my API!");
 });
 
+app.get("/employees", (req, res) => {
+  getData(async (db) => {
+    const employees = await db.collection("employees").find().toArray();
+    res.json(employees);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server runs on http://localhost:${port} `);
 });
+
+async function getData(done) {
+  await client.connect();
+  const db = client.db("northwind");
+  done(db);
+}
